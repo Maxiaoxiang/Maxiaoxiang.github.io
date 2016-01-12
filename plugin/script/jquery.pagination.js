564845fedae489c76fe1c69a0dd7aab218898d31
马晓翔 <251445460@qq.com>
2016-01-12 18:00:27 +0800

    Updated 分页

--- plugin/script/jquery.pagination.js
+++ plugin/script/jquery.pagination.js
@@ -21,6 +21,9 @@
 		homePage:'',			//首页节点内容
 		endPage:'',				//尾页节点内容
 		count:3,				//当前页前后分页个数
+		jump:false,				//跳转到指定页数
+		jumpIptCls:'jump-ipt',	//文本框内容
+		jumpBtnCls:'jump-btn',	//跳转按钮
 		callback:function(){}	//回调
 	};
 
@@ -28,6 +31,7 @@
 		//全局变量
 		var opts = options,//配置
 			current,//当前页
+			$document = $(document),
 			$obj = $(element);//容器
 
 		//获取总页数
@@ -77,23 +81,50 @@
 				$obj.find('.'+opts.nextCls) && $obj.find('.'+opts.nextCls).remove();
 			}
 
+			html += opts.jump ? '<input type="text" class="'+opts.jumpIptCls+'"><a href="javascript:;" class="'+opts.jumpBtnCls+'">跳转</a>' : '';
+
 			$obj.empty().html(html);
 		};
 
 		//绑定事件
 		this.eventBind = function(){
 			var self = this;
+			var pageCount = this.getTotalPage();//总页数
 			$obj.on('click','a',function(){
 				if($(this).hasClass(opts.nextCls)){
 					var index = parseInt($obj.find('.'+opts.activeCls).text()) + 1;
 				}else if($(this).hasClass(opts.prevCls)){
 					var index = parseInt($obj.find('.'+opts.activeCls).text()) - 1;
+				}else if($(this).hasClass(opts.jumpBtnCls)){
+					if($obj.find('.'+opts.jumpIptCls).val() !== ''){
+						var index = parseInt($obj.find('.'+opts.jumpIptCls).val());
+					}else{
+						return;
+					}
 				}else{
 					var index = parseInt($(this).data('page'));
 				}
 				self.filling(index);
 				typeof opts.callback === 'function' && opts.callback(index);
 			});
+			$obj.on('input propertychange','.'+opts.jumpIptCls,function(){
+				var val = $(this).val();
+				var reg = /[^\d]/g;
+	            if (reg.test(val)) {
+	                $(this).val(val.replace(reg, ''));
+	            }
+	            (parseInt(val) > pageCount) && $(this).val(pageCount);
+	            if(parseInt(val) === 0){//最小值为1
+	            	$(this).val(1);
+	            }
+			});
+			$document.keydown(function(e){ 
+		        if(e.keyCode == 13 && $obj.find('.'+opts.jumpIptCls).val()){
+		        	var index = parseInt($obj.find('.'+opts.jumpIptCls).val());
+		            self.filling(index);
+					typeof opts.callback === 'function' && opts.callback(index);
+		        }
+		    });
 		};
 
 		//初始化
