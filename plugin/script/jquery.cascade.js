49269850fc0a11c0dd51bc7827257750fd6b8740
马晓翔 <251445460@qq.com>
2016-04-08 15:55:19 +0800

    Updated

---
+++ plugin/script/jquery.cascade.js
@@ -0,0 +1,102 @@
+/**
+ * cascade瀑布流插件
+ * @version 0.0.1
+ * @url http://www.maxiaoxiang.com
+ * @E-mail 251445460@qq.com
+ */
+;(function($,window,document,undefined){
+
+	//配置参数
+	var defaults = {
+		liCls: 'item',			//排列元素
+		level: '10',			//间距
+		vertical: '10',			//垂直
+		callback: function(){}	//回调
+	};
+
+	var Cascade = function(element,options){
+		//全局变量
+		var opts = options,//配置
+			$document = $(document),
+			$w = $(window),
+			$obj = $(element);//容器
+
+		//排列
+		this.arrangement = function(){
+			var box_w = $obj.width(),
+				$li = $('.'+opts.liCls),
+				li_w = $li.eq(0).width() + Number(opts.level),
+				column = Math.floor(box_w / li_w),
+				len = $li.length,
+				first_arr = [],
+				li_arr = [];
+			// $obj.width(li_w * column - Number(opts.level));
+			$li.each(function(){
+				li_arr.push($(this).height());
+			});
+			for(var i = 0; i < column; i++){
+				$li.eq(i).css({
+					'position': 'absolute',
+					'top': '0',
+					'left': li_w * i + 'px'
+				});
+				first_arr.push($li.eq(i).height());
+			}
+			for(var i = column; i < len; i++){
+				var index = this.getMinIndex(first_arr);
+				$li.eq(i).css({	
+					'position': 'absolute',
+					'top': first_arr[index] + Number(opts.vertical) + 'px',
+					'left': li_w * index + 'px'
+				});
+				first_arr[index] = li_arr[i] + first_arr[index] + Number(opts.vertical);
+			}
+		};
+		//获取最小高度下标
+		this.getMinIndex = function(arr){
+			var a = arr[0];
+			var index = 0;
+			for (var i in arr) {
+				if (arr[i] < a) {
+					a = arr[i];
+					index = i;
+				}
+			}
+			return index;
+		};
+		//窗口变化
+		this.resize = function(){
+			clearTimeout(time);
+			var self = this,
+				time = setTimeout(function(){
+					$w.resize(function(){
+						self.arrangement();
+					});
+				},500);
+		};
+		//初始化
+		this.init = function(){
+			this.arrangement();
+			this.resize();
+		};
+		this.init();
+	};
+
+	$.fn.cascade = function(parameter,callback){
+		if(typeof parameter == 'function'){
+			callback = parameter;
+			parameter = {};
+		}else{
+			parameter = parameter || {};
+			callback = callback || function(){};
+		}
+		var options = $.extend({},defaults,parameter);
+		return this.each(function(){
+			var cascade = new Cascade(this,options);
+			callback(cascade);
+		});
+	};
+
+	return $;
+
+})(jQuery,window,document);
